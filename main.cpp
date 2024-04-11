3f39ce9f25af9df2decef88391a53402bc7032f6
kim <126642836+mellonmusk@users.noreply.github.com>
2024-04-12 00:02:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-  int n;
-  cin>>n;
-  int profit=0, day[n], pay[n];
-  for(int i=0;i<n;i++){
-    cin >>day[i]>>pay[i];
-  }
-  int maxProfit=0;
-  for(int start=0;start<n;start++){
-    int i=start;
-    for(;i+day[i]<n+1;){
-      profit+=pay[i];
-      i=i+day[i];
-    }
-    if(profit>maxProfit) maxProfit=profit;
-    profit=0;
-  }
-  cout<<maxProfit;
+  cout<<"Hello world!\n";
   return 0;
 }
